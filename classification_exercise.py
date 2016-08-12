5c3a0249db0b3f22a436f2f7b8eb0658ee6af46e
Russell Smith <rws2148@columbia.edu>
2016-08-12 12:45:43 -0400

    same idea

--- classification_exercise.py
+++ classification_exercise.py
@@ -12,10 +12,10 @@ digits_y = digits.target
 np.random.seed(0)
 indices = np.random.permutation(len(digits_X))
 
-digits_X_train = digits_X[indices[:-10]]
-digits_y_train = digits_y[indices[:-10]]
-digits_X_test  = digits_X[indices[-10:]]
-digits_y_test  = digits_y[indices[-10:]]
+digits_X_train = digits_X[:.9 * len(digits_X)]
+digits_y_train = digits_y[:.9 * len(digits_X)]
+digits_X_test  = digits_X[.9 * len(digits_X):]
+digits_y_test  = digits_y[.9 * len(digits_X):]
 
 logistic = linear_model.LogisticRegression(C=1e5)
 logistic.fit(digits_X_train, digits_y_train)
