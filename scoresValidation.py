10ca7a5734e1a8de9d42be2fa3256ad611f38511
Russell Smith <rws2148@columbia.edu>
2016-08-15 10:58:22 -0500

    validation

---
+++ scoresValidation.py
@@ -0,0 +1,24 @@
+from sklearn import datasets, svm
+
+digits = datasets.load_digits()
+X_digits = digits.data
+y_digits = digits.target
+svc = svm.SVC(C=1, kernel='linear')
+
+print(svc.fit(X_digits[:-100], y_digits[:-100]).score(X_digits[-100:], y_digits[-100:]))
+
+import numpy as np
+X_folds = np.array_split(X_digits, 3)
+y_folds = np.array_split(y_digits, 3)
+scores = list()
+for k in range(3):
+    # We use 'list' to copy, in order to 'pop' later on
+    X_train = list(X_folds)
+    X_test  = X_train.pop(k)
+    X_train = np.concatenate(X_train)
+    y_train = list(y_folds)
+    y_test  = y_train.pop(k)
+    y_train = np.concatenate(y_train)
+    scores.append(svc.fit(X_train, y_train).score(X_test, y_test))
+
+print(scores)
