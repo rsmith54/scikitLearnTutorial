6772edb17ded27c0c62a636370698395e4762812
Russell Smith <rws2148@columbia.edu>
2016-08-16 14:36:19 -0500

    vector quantization

---
+++ vectorQuantization.py
@@ -0,0 +1,32 @@
+from sklearn import cluster, datasets
+import numpy as np
+
+iris = datasets.load_iris()
+X_iris = iris.data
+y_iris = iris.target
+
+k_means = cluster.KMeans(n_clusters=3)
+k_means.fit(X_iris)
+
+print(k_means.labels_[::10])
+print(y_iris[::10])
+
+import scipy as sp
+try:
+       face = sp.face(gray=True)
+except AttributeError:
+    from scipy import misc
+    face = misc.face(gray=True)
+
+X = face.reshape((-1, 1)) # We need an (n_sample, n_feature) array
+k_means = cluster.KMeans(n_clusters=5, n_init=1)
+k_means.fit(X)
+
+values = k_means.cluster_centers_.squeeze()
+labels = k_means.labels_
+face_compressed = np.choose(labels, values)
+face_compressed.shape = face.shape
+
+print(values)
+print(labels)
+print(face_compressed)
