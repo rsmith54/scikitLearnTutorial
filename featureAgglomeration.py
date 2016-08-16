823c5810a174cde26519d6ec10b92ad196bb2493
Russell Smith <rws2148@columbia.edu>
2016-08-16 14:45:14 -0500

    ft agg

---
+++ featureAgglomeration.py
@@ -0,0 +1,17 @@
+from sklearn import datasets,cluster
+import numpy as np
+from sklearn.feature_extraction.image import grid_to_graph
+
+digits       = datasets.load_digits()
+images       = digits.images
+X            = np.reshape(images, (len(images), -1))
+connectivity = grid_to_graph(*images[0].shape)
+
+agglo = cluster.FeatureAgglomeration(connectivity=connectivity,n_clusters=32)
+
+agglo.fit(X)
+
+X_reduced = agglo.transform(X)
+
+X_approx = agglo.inverse_transform(X_reduced)
+images_approx = np.reshape(X_approx, images.shape)
